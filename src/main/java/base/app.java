794bd7e85f86ed87c3d5972f05070f541feb8a80
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 23:17:10 -0400

    Update app.java

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -6,24 +6,57 @@
 
 
 package base;
+
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class app {
-    static Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
-        double LBAC = 0.08;
-        double ounces =0;
-        double weight = 0;
-        double hours = 0;
 
+    public static void main(String[] args) {
+
+        double LBAC = 0.08;
+        double measure = 5.14;
+        double measure2 = 0.15;
+        double ounces;
+        double weight;
+        int hours;
+        double women = .73;
+        double men = .66;
+        int gender;
+        Scanner input = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#0.00000");
         System.out.println("Enter a 1 if you are male or a 2 if you are female: ");
+        gender = input.nextInt();
 
         System.out.println("How many ounces of alcohol did you have? ");
-        ounces = Integer.parseInt(in.nextLine());
+        ounces = input.nextInt();
 
         System.out.println("What is your weight in pounds? ");
-        weight = Double.parseDouble(in.nextLine());
+        weight = input.nextDouble();
 
         System.out.println("How many hours has it been since your last drink?");
-        hours = Double.parseDouble(in.nextLine());
+        hours = input.nextInt();
+        double BAC1 = (ounces * measure) / (weight * women) - (measure2 * hours);
+        double BAC2 = ounces * measure / (weight * men) - (measure2 * hours);
+        if (gender == 1){
+
+            if(BAC2 < LBAC)
+            {
+                System.out.println("Your BAC is " + formatter.format(BAC1) +"\nIt is legal for you to drive");
+    }
+    if(BAC1 > LBAC)
+    {
+        System.out.println("Your BAC is " +formatter.format(BAC2) +"\nIt is not legal for you to drive");
+    }
+}
+        if(gender == 2){
+            if(BAC1 < LBAC)
+            {
+                System.out.println("Your BAC is " +formatter.format(BAC1) + "\nIt is legal for you to drive");
+            }
+            if(BAC2 > LBAC)
+            {
+                System.out.println("Your BAC is " + formatter.format(BAC1) + "\nIt is not legal for you to drive");
+            }
+        }
     }
 }
